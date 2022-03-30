3faea5c19b2477087d8782593136f3792367b0a0
ManoellaLinoe <mlino@grupocereal.com.br>
2022-03-30 08:15:24 -0300

    Problema resolvido

--- src/application/UI.java
+++ src/application/UI.java
@@ -25,12 +25,12 @@ public class UI {
 	
 	private static void printPiece (ChessPiece piece) {
 		if (piece == null) {
-			System.out.println("-");
+			System.out.print("-");
 		}
 		else {
-			System.out.println(piece);
+			System.out.print(piece);
 		}
-		System.out.println(" ");
+		System.out.print(" ");
 	}
 	
 }
