c0a4ab31f144bd0adaa6be346c34e072209a205d
ManoellaLinoe <mlino@grupocereal.com.br>
2022-04-04 01:18:45 -0300

    Possible moves of a piece - Comitando parte esquecida

--- src/application/UI.java
+++ src/application/UI.java
@@ -55,7 +55,7 @@ public class UI {
 				return new ChessPosition(column, row);
 			}
 			catch (RuntimeException e) {
-				throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
+				throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8!");
 			}
 		}
 			
