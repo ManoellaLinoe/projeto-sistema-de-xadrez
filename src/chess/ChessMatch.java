605c3ec1ced668036b3be6edd7e8b3a6d6ade38f
ManoellaLinoe <mlino@grupocereal.com.br>
2022-04-04 01:40:55 -0300

    Implementing possible moves of rook - Arquivos não salvos

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -15,6 +15,13 @@ public class ChessMatch {
 	       initialSetup();
 	}
 	
+	
+	
+	
+	
+	
+	
+	
 	public ChessPiece [][] getPieces () {
 		ChessPiece [][] mat = new ChessPiece[board.getRows()] [board.getColumns()];
 		for (int i = 0; i < board.getRows(); i++) {
@@ -26,13 +33,27 @@ public class ChessMatch {
 	}
 	
 	
+	
+	
+	
+	
+	
+	
 	public ChessPiece performChessMove (ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece) capturedPiece;
 	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	private Piece makeMove (Position source, Position target) {
@@ -41,6 +62,13 @@ public class ChessMatch {
 		board.placePiece(p, target);
 		return capturedPiece;
 	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	private void validateSourcePosition (Position position) {
@@ -55,9 +83,29 @@ public class ChessMatch {
 	
 	
 	
+	
+	
+	private void validateTargetPosition (Position source, Position target) {
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException ("The chosen piece can't move to target position!");
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
+	
+	
+	
+	
+	
+	
 	
 	
 	
