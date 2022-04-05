478595e2f773e5f2a8355e7d6c9c5ede6a910fce
ManoellaLinoe <mlino@grupocereal.com.br>
2022-04-05 03:10:23 -0300

    Queen and Pawn bug

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -6,34 +6,16 @@ import chess.ChessPiece;
 import chess.Color;
 
 public class Pawn extends ChessPiece {
-	
-	
-	
-	
-	
 
 	public Pawn(Board board, Color color) {
 		super(board, color);
 	}
 
-	
-	
-	
-	
-	
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		
-		
-		
-		
-		
 		Position p = new Position(0, 0);
-		
-		
-		
-		
 
 		if (getColor() == Color.WHITE) {
 			p.setValues(position.getRow() - 1, position.getColumn());
@@ -60,7 +42,7 @@ public class Pawn extends ChessPiece {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 			p.setValues(position.getRow() + 2, position.getColumn());
-			Position p2 = new Position(position.getRow() - 1, position.getColumn());
+			Position p2 = new Position(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
@@ -75,10 +57,6 @@ public class Pawn extends ChessPiece {
 		}
 		return mat;
 	}
-	
-	
-	
-	
 	
 	@Override
 	public String toString() {
